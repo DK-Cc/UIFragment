c020151b471bbfac8689df675dd64fa8ba5b178f
cong.chen <cong.chen@ck-telecom.com>
2017-08-04 18:32:39 +0800

    Initial commit

---
+++ app/src/main/java/com/ckt/cc/uifragment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ckt.cc.uifragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    private void addFragment() {
+        
+    }
+
+
+}
